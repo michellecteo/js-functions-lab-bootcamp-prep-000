2e895d08e375e46a23f3c34abbe436703d99d7d8
Michelle Teo <michelle.c.teo@gmail.com>
2019-07-24 22:26:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!'
 }
 happyHolidaysTo('you');
+
+
+  describe('happyHolidaysTo(name)', () => {
+    it('returns "Happy holidays, ${name}!"', () => {
+      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
+    })
+  })
